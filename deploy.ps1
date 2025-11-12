f25a75a92bf9125991a86b2b133d143e1d609817
Jake Poshepny <jposhepny99@gmail.com>
2025-11-11 23:16:40 -0600

    Pre Deploy

--- deploy.ps1
+++ deploy.ps1
@@ -3,7 +3,7 @@ git add .
 git commit -m "Pre Deploy"
 git push
 
-# Build
+# Ensure we are on Main and Build the Project
 git checkout main
 ng build
 
@@ -13,9 +13,10 @@ mkdir ../temp-gh-pages
 robocopy ./dist ../temp-gh-pages /E /XD browser
 robocopy ./dist/browser ../temp-gh-pages /E
 
+# Duplicate index.html and rename it to 404.html
 Copy-Item -Path ../temp-gh-pages/index.html -Destination ../temp-gh-pages/404.html -Force
 
-# Deploy to GH Pages
+# Switch to GH Pages Branch
 git checkout gh-pages
 
 Get-ChildItem -Path . -Force | Where-Object { @('.angular', '.git', '.idea', 'dist', 'node_modules', '.gitignore') -notcontains $_.Name } | Remove-Item -Recurse -Force
